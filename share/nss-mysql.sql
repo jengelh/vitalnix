03f8454b09535dc2f455f83e45ce8352cf2cfe2b
Jan Engelhardt <jengelh@computergmbh.de>
2006-09-09 21:36:57 +0000

    Change to localhost, add Vitalnix note

--- share/nss-mysql.sql
+++ share/nss-mysql.sql
@@ -1,12 +1,17 @@
-# Permission scheme for nss_mysql:
+# Usual permission scheme for nss_mysql
 
-grant select on nss_mysql.users to 'nss'@'%';
-grant select on nss_mysql.groups to 'nss'@'%';
-grant select on nss_mysql.groupmap to 'nss'@'%';
+# Note that the hostname wildcard '%' does NOT include localhost.
+grant select on nss_mysql.users to 'nss'@'localhost';
+grant select on nss_mysql.groups to 'nss'@'localhost';
+grant select on nss_mysql.groupmap to 'nss'@'localhost';
 
-grant select on nss_mysql.* to 'nss-shadow'@'%' identified by 'foobar';
+grant select on nss_mysql.* to 'nss-shadow'@'localhost' identified by 'foobar';
 
 # Following line is useful for Vitalnix, whose db-mysql.conf will, by default,
 # source /etc/nss-mysql*.conf.
 #grant select, insert, update, lock tables, execute 
-#on nss_mysql.* to 'nss-shadow'@'%';
+#on nss_mysql.* to 'nss-shadow'@'localhost';
+#
+# However, you should configure Vitalnix so that it uses a separate account:
+#grant select, insert, update, lock tables, execute
+#on nss_mysql.* to 'nss-vitalnix'@'localhost';
