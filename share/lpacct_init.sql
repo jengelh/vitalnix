5e274b6d9f68dbf6b8e5934a9245a64b88ce2134
Jan Engelhardt <jengelh@computergmbh.de>
2006-11-06 06:57:51 +0000

    Use default precision; add page counting

--- share/lpacct_init.sql
+++ share/lpacct_init.sql
@@ -4,14 +4,15 @@ create database lpacct;
 use lpacct;
 create table printlog (
     time        timestamp       not null        default current_timestamp,
-    jid         int(11) unsigned,
+    jid         int unsigned,
     queue       varchar(64),
     user        varchar(64),
-    cyan        double(9, 5)          not null        default 0,
-    magenta     double(9, 5)          not null        default 0,
-    yellow      double(9, 5)          not null        default 0,
-    black       double(9, 5)          not null        default 0,
-    total       double(9, 5)          not null        default 0,
+    cyan        double          not null        default 0,
+    magenta     double          not null        default 0,
+    yellow      double          not null        default 0,
+    black       double          not null        default 0,
+    total       double          not null        default 0,
+    pages       int unsigned    not null        default 0,
     index       user (user)
 ) default charset=utf8;
 
