31ccfb5dd856c76dbbd2f742470c58fc7fc13f8e
Jan Engelhardt <jengelh@computergmbh.de>
2006-11-18 11:08:25 +0000

    nss-mysql: add a few blank comment lines

--- share/nss-mysql.sql
+++ share/nss-mysql.sql
@@ -9,9 +9,14 @@ grant select on nss_mysql.* to 'nss-shadow'@'localhost' identified by 'foobar';
 
 # Following line is useful for Vitalnix, whose db-mysql.conf will, by default,
 # source /etc/nss-mysql*.conf.
+#
+
 #grant select, insert, update, lock tables, execute
 #on nss_mysql.* to 'nss-shadow'@'localhost';
-#
+
+
 # However, you should configure Vitalnix so that it uses a separate account:
+#
+
 #grant select, insert, update, lock tables, execute
 #on nss_mysql.* to 'nss-vitalnix'@'localhost';
