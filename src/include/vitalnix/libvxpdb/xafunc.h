4b90895cf0821b5b985b8d9c5ebf42f44e4f7e61
Jan Engelhardt <jengelh@computergmbh.de>
2007-08-28 22:10:30 +0000

    Add new interface functions

--- src/include/vitalnix/libvxpdb/xafunc.h
+++ src/include/vitalnix/libvxpdb/xafunc.h
@@ -120,6 +120,10 @@ static inline int vxpdb_getpwnam(struct vxpdb_state *state, const char *user,
     struct vxpdb_user *result)
 {
 	struct vxpdb_user mask;
+
+	if (state->vtable->getpwnam != NULL)
+		return state->vtable->getpwnam(state, user, result);
+
 	vxpdb_user_clean(&mask);
 	mask.pw_name = const_cast(char *, user);
 	return vxpdb_userinfo(state, &mask, result, result != NULL);
@@ -129,6 +133,10 @@ static inline int vxpdb_getpwuid(struct vxpdb_state *state, long uid,
     struct vxpdb_user *result)
 {
 	struct vxpdb_user mask;
+
+	if (state->vtable->getpwuid != NULL)
+		return state->vtable->getpwuid(state, uid, result);
+
 	vxpdb_user_clean(&mask);
 	mask.pw_uid = uid;
 	return vxpdb_userinfo(state, &mask, result, result != NULL);
@@ -138,6 +146,10 @@ static inline int vxpdb_getgrnam(struct vxpdb_state *state, const char *group,
     struct vxpdb_group *result)
 {
 	struct vxpdb_group mask;
+
+	if (state->vtable->getgrnam != NULL)
+		return state->vtable->getgrnam(state, group, result);
+
 	mask.gr_name = const_cast(char *, group);
 	mask.gr_gid  = PDB_NOGID;
 	return vxpdb_groupinfo(state, &mask, result, result != NULL);
@@ -147,6 +159,10 @@ static inline int vxpdb_getgrgid(struct vxpdb_state *state, long gid,
     struct vxpdb_group *result)
 {
 	struct vxpdb_group mask;
+
+	if (state->vtable->getgrgid != NULL)
+		return state->vtable->getgrgid(state, gid, result);
+
 	mask.gr_name = NULL;
 	mask.gr_gid  = gid;
 	return vxpdb_groupinfo(state, &mask, result, result != NULL);
