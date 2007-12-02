bc5ffa57fc5149f40c1c09002cc8840940d269de
Jan Engelhardt <jengelh@computergmbh.de>
2007-12-02 17:56:37 +0000

    Update tcrypt testsuite

--- src/devutil/tcrypt.c
+++ src/devutil/tcrypt.c
@@ -1,6 +1,6 @@
 /*
- *	libvxutil/crypt.c - Password encryption
- *	Copyright © CC Computer Consultants GmbH, 2003 - 2007
+ *	tcrypt - Test encryption
+ *	Copyright © CC Computer Consultants GmbH, 2007
  *	Contact: Jan Engelhardt <jengelh [at] computergmbh de>
  *
  *	This file is part of Vitalnix. Vitalnix is free software; you
@@ -9,16 +9,19 @@
  *	Foundation; either version 2.1 or 3 of the License.
  */
 #include <stdio.h>
+#include <stdlib.h>
 #include <vitalnix/libvxutil/libvxutil.h>
 
 int main(int argc, const char **argv)
 {
-	char *out;
+	char *bf_out, *nt_out;
 
 	while (*++argv != NULL) {
-		vxutil_cryptpw(*argv, NULL, CRYPW_SMBNT, &out);
-		printf("%s\n", out);
-		free(out);
+		vxutil_cryptpw(*argv, NULL, CRYPW_BLOWFISH, &bf_out);
+		vxutil_cryptpw(*argv, NULL, CRYPW_SMBNT, &nt_out);
+		printf("%s\t%s\n\t%s\n", *argv, bf_out, nt_out);
+		free(bf_out);
+		free(nt_out);
 	}
 
 	return EXIT_SUCCESS;
